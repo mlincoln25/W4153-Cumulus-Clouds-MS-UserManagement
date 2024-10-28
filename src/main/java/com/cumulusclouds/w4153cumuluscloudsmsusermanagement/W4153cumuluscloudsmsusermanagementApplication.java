737218cc288bd7fc44f2ee69e58cc8dad68e3278
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-28 02:37:48 -0400

    chore: initial commit

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsusermanagement/W4153cumuluscloudsmsusermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4153cumuluscloudsmsusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4153cumuluscloudsmsusermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W4153cumuluscloudsmsusermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumulusclouds/w4153cumuluscloudsmsusermanagement/W4153cumuluscloudsmsusermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4153cumuluscloudsmsusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W4153cumuluscloudsmsusermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
