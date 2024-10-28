4ea75086cabbedf693bcb60814696153706c9349
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-28 02:57:10 -0400

    build: user controller

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsusermanagement/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.cumulusclouds.w4153cumuluscloudsmsusermanagement.controller;
+
+import com.cumulusclouds.w4153cumuluscloudsmsusermanagement.model.User;
+import com.cumulusclouds.w4153cumuluscloudsmsusermanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
