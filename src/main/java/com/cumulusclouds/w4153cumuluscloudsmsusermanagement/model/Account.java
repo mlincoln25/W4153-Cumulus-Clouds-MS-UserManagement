75980097c263d9e052f000bf787621179379d109
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-30 02:38:29 -0400

    build: Account object with annotations to match the db

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsusermanagement/model/Account.java
@@ -0,0 +1,35 @@
+package com.cumulusclouds.w4153cumuluscloudsmsusermanagement.model;
+
+import java.util.UUID;
+import java.util.List;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "account")
+public class Account {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "user_id", updatable = false, nullable = false)
+  private UUID userId;
+
+  @Column(name = "username", length = 50, nullable = false)
+  private String username;
+
+  @Column(name = "email", length = 100, nullable = false, unique = true)
+  private String email;
+
+  @Column(name = "password_hash", length = 255, nullable = false)
+  private String passwordHash;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private java.time.Instant createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private java.time.Instant updatedAt;
+  
+}
